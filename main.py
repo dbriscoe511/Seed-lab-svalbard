d99ee1b58e2c73b0dc36f9c44f8eb0c2c8620813
dbriscoe511 <extradrivespace123456@gmail.com>
2021-09-27 13:09:37 -0600

    changed to realative angle sending, in encoder pulses

--- main.py
+++ main.py
@@ -8,7 +8,14 @@ while True:
     angle = 2
     measured_angle = 0
     time.sleep(0.5)
+
     if not angle == previous_angle:
+        if(angle-previous_angle>0):
+            encoderangle = 64*(angle-previous_angle)
+        else:
+            encoderangle = 64*(angle-previous_angle)+255
+
+        
         comms.send(angle)
         time.sleep(0.5)
         measured_angle = comms.read()
