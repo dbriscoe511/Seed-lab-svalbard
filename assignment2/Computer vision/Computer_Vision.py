4e36d887aed100c71c04fa3d8865d4a407901087
Canned-Lettuce <CannedLettuce@tutanota.com>
2021-09-27 13:44:17 -0600

    Calibration and location detection work for yellow hexagon. Next step is doing it for the blue tape.

--- assignment2/Computer vision/Computer_Vision.py
+++ assignment2/Computer vision/Computer_Vision.py
@@ -6,21 +6,31 @@ import cv2 as cv
 from matplotlib import pyplot as plt
 
 camera = PiCamera()
-
+gains = []
 #calibration
 camera.start_preview()
-sleep(2)
+sleep(3)
+camera.stop_preview()
+
+for i in range(10):
+    #camera.capture('img' + str(i)+'.png')
+    gains.append(camera.awb_gains)
+    print("img " + str(i))
+print(gains)
+    
+g0 = np.mean(gains[0])
+g1 = np.mean(gains[1])
+print(str(g0) + ", " + str(g1))
+
 
 camera.shutter_speed = camera.exposure_speed
 camera.exposure_mode = 'off'
-g = camera.awb_mode = 'off'
-camera.awb_gains = g
-camera.capture_sequence(['image%02d.png' % i for i in range(10)])
-camera.stop_preview()
+camera.awb_mode = 'off'
+camera.awb_gains = [g0, g1]
 
 
 
-
+sleep(2)
 
 
 #Takes a picture and stores to file
@@ -51,11 +61,25 @@ _, contours, hierarchy = cv.findContours(thresh,cv.RETR_TREE,cv.CHAIN_APPROX_SIM
 cnt = contours[0]
 M = cv.moments(cnt)
 cx = int(M['m10']/M['m00'])
+cy = int(M['m01']/M['m00'])
 #Finds distance between object center and image center
-dis = abs(img.shape[1]/2-cx)
+dis = img.shape[1]/2-cx
 leastDis = min(dis, cx)
 fov = 53.5
 anglePerPix = fov / img.shape[1]
 #Used the 2 centers and field of view per pixel to find the angle between the object and camera
 angle = anglePerPix * dis
 print("Angle is: " + str(angle))
+
+if angle < -13.5:
+    print("1")
+elif -13.5 < angle < 0:
+    print("2")
+elif 0 < angle < 13.5:
+    print("3")
+elif 13.5 < angle:
+    print("4")
+im2 = cv.cvtColor(binary, cv.COLOR_GRAY2BGR)
+im2 = cv.circle(im2, (cx, cy), 5, (0,255,0), 2)
+plt.imshow(im2)
+plt.show()
