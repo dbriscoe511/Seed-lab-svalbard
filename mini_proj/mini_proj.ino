8d54dafb833d729de7b82bf1b29d50c430f11dc7
= <extradrivespace123456@gmail.com>
2021-09-29 12:37:56 -0600

    added arduino code for basic i2c test

---
+++ mini_proj/mini_proj.ino
@@ -0,0 +1,28 @@
+  #include <Wire.h>
+  int c = 0;
+  void setup()
+  {
+    Wire.begin(4);                // join i2c bus with address #4
+    Wire.onReceive(receive_e); // register event
+    Wire.onRequest(sendData);
+    Serial.begin(9600);           // start serial for output
+  }
+  
+  void loop()
+  {
+    delay(100);
+  }
+  
+  void receive_e(int events)
+  {
+    
+    c = Wire.read();
+    Serial.println(c);
+    
+    
+  }
+  void sendData()
+  {
+    Wire.write(c);
+  }
+    
