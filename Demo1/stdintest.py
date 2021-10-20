3a4b67bf55a7bb4fb66a0f2a34b1ce022753ef51
dbriscoe511 <extradrivespace123456@gmail.com>
2021-10-20 13:28:42 -0600

    stdin will not work.

--- Demo1/stdintest.py
+++ Demo1/stdintest.py
@@ -1,2 +1,10 @@
 import sys
-print(sys.stdin.readline())
+import time
+i=0
+while True:
+    i += 1
+    sys.stdout.write("\n"+str(i))
+    time.sleep(1)
+    print("\nreading ")
+
+    print(str(sys.stdin.read()))
