bdabc918006568aa1eba3e17738788e2fe56c27a
dbriscoe511 <extradrivespace123456@gmail.com>
2021-10-22 12:08:04 -0600

    added cv_example code to main for testing

--- Demo1/main.py
+++ Demo1/main.py
@@ -1,9 +1,13 @@
 import time
 import comms
 import sys
+import computer_vision as cv
+import subprocess
 
 system = comms.comm() # initialize i2c and display
 
+cmd = [sys.executable, "-c", "import computer_vision as cv; gains = cv.camera_setup(); cv.cv_main(gains)"]
+process = subprocess.Popen(cmd, stdout=subprocess.PIPE)
 #These values are hard coded into the arduino on demo1, no need for this function for the first demo. 
 def excersize2(angle,dist):
     time.sleep(8)
@@ -14,8 +18,11 @@ def excersize2(angle,dist):
 def excersize1():
     while (True):
         time.sleep(0.1)
-        t_angle = sys.stdin.readline()
-        system.update_lcd(str(t_angle))
+        angle = process.stdout.readline()
+        angle = angle.decode('utf-8')
+        angle.strip('\n')
+        print(angle)
+        system.update_lcd(str(angle))
 
 exr = int(input("what excersize? (1: camera angle test, 2 (obsolete): rotate/drive test)"))
 if exr ==2:
