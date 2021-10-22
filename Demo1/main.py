16f7726f6480d79e9d25ec8d8fbdb8b7edd62a3b
dbriscoe511 <extradrivespace123456@gmail.com>
2021-10-22 12:01:34 -0600

    commented code

--- Demo1/main.py
+++ Demo1/main.py
@@ -4,6 +4,7 @@ import sys
 
 system = comms.comm() # initialize i2c and display
 
+#These values are hard coded into the arduino on demo1, no need for this function for the first demo. 
 def excersize2(angle,dist):
     time.sleep(8)
     system.angle(angle)
@@ -16,7 +17,7 @@ def excersize1():
         t_angle = sys.stdin.readline()
         system.update_lcd(str(t_angle))
 
-exr = int(input("what excersize? (1: camera angle test, 2: rotate/drive test)"))
+exr = int(input("what excersize? (1: camera angle test, 2 (obsolete): rotate/drive test)"))
 if exr ==2:
     #angle(degree), dist
     excersize2(90,5)
