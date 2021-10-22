42c1f64e6945160107e02ad19c3c9974a1cd7a14
Canned-Lettuce <CannedLettuce@tutanota.com>
2021-10-22 12:16:48 -0600

    demo1 works now

--- Demo1/main.py
+++ Demo1/main.py
@@ -6,8 +6,7 @@ import subprocess
 
 system = comms.comm() # initialize i2c and display
 
-cmd = [sys.executable, "-c", "import computer_vision as cv; gains = cv.camera_setup(); cv.cv_main(gains)"]
-process = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+
 #These values are hard coded into the arduino on demo1, no need for this function for the first demo. 
 def excersize2(angle,dist):
     time.sleep(8)
@@ -16,8 +15,10 @@ def excersize2(angle,dist):
     system.move(dist*12)
 
 def excersize1():
-    while (True):
-        time.sleep(0.1)
+    cmd = [sys.executable, "-c", "import computer_vision as cv; gains = cv.camera_setup(); cv.cv_main(gains)"]
+    process = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+    while process.poll() is None:
+        time.sleep(0.5)
         angle = process.stdout.readline()
         angle = angle.decode('utf-8')
         angle.strip('\n')
