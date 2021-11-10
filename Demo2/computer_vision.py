5913d2d34d15cba33e800655978622c65e6495ba
dbriscoe511 <extradrivespace123456@gmail.com>
2021-11-10 14:46:32 -0700

    added cv file

--- Demo2/computer_vision.py
+++ Demo2/computer_vision.py
@@ -6,24 +6,10 @@ import cv2
 import io
 import sys
 
-'''
-This script handles a picam and sends its coordonites to a screen
-
-It should be opened as a subprocess to keep the video running,
- it can be setup and called with the folllowing syntax:
-
-cmd = [sys.executable, "-c", "import computer_vision as cv; gains = cv.camera_setup(); cv.cv_main(gains)"]
-process = subprocess.Popen(cmd, stdout=subprocess.PIPE)
-while process.poll() is None:
-        time.sleep(0.5)
-        angle = process.stdout.readline()
-
-'''
-
-def camera_setup():  #Calculates the white balance to be passed to cv_main()
+def camera_setup():
     camera = PiCamera()
     camera.resolution = (640, 480)
-    camera.framerate = 50
+    camera.framerate = 15
 
     gains = []
     camera.start_preview()
@@ -38,7 +24,7 @@ def camera_setup():  #Calculates the white balance to be passed to cv_main()
     return gains
 def cv_main(gains):
     
-    camera = PiCamera()  #Set up camera with passed values
+    camera = PiCamera()
     camera.resolution = (640, 480)
     camera.framerate = 50
     g0 = np.mean(gains[0])
@@ -50,7 +36,7 @@ def cv_main(gains):
     sleep(0.1)
     
     for foo in camera.capture_continuous(stream, format='bgr', use_video_port=True):
-        stream.truncate() #Truncates the video to ensure the correct aspect ratio
+        stream.truncate()
         stream.seek(0)
         frame = stream.array
         
@@ -59,29 +45,65 @@ def cv_main(gains):
         upper = np.array([130, 255, 255])
         
         mask = cv2.inRange(hsv, lower, upper)
-        results = cv2.bitwise_and(frame, frame, mask=mask) #Masks image to isolate blue
+        results = cv2.bitwise_and(frame, frame, mask=mask)
         
-        kernel = np.ones((5,5),np.uint8)  #Cleans up masked image with morphological transformations 
+        kernel = np.ones((5,5),np.uint8)
         closed = cv2.morphologyEx(results, cv2.MORPH_CLOSE, kernel)
         smoothed = cv2.medianBlur(closed,5)
+        #smoothed = cv2.dilate(smoothed, kernel, iterations=3)
+        smoothed = smoothed[300:480, 0:640]
         
         grayscale = cv2.cvtColor(smoothed, cv2.COLOR_BGR2GRAY)     # Converts to grayscale
         
-        ret,thresh = cv2.threshold(grayscale,50,255,cv2.THRESH_BINARY)   # Performs thresholding
+        ret,thresh = cv2.threshold(grayscale,0,255,cv2.THRESH_BINARY)   # Performs thresholding
         
-        arr = np.array(thresh)       # Converts threshold to numPy
-        arr = np.nonzero(arr)        # Nonzeros the numPy array
-        locateX = np.mean(arr[1])    # Finds center of marker
-        locateY = np.mean(arr[0])
         
-        angle = 27*(locateX - 320)/320    # Finds angle needed to turn
         
-        sys.stdout.write(str(angle) + '\n') #Writes angle to console to be read by external program
+        cont_img, contours, hierarchies, = cv2.findContours(thresh, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+        
+        cX = None
+        cY = None
+        
+        if len(contours) != 0:
+            contours = max(contours, key = cv2.contourArea)
+        
+        if len(contours) != 0:
+            cv2.drawContours(smoothed, contours, -1, (0, 255, 0), 2)
+            #for c in contours:
+            M = cv2.moments(contours)
+            if M['m00'] != 0:
+                cX = int(M['m10'] / M['m00'])
+                cY = int(M['m01'] / M['m00'])
+            
+            if (cX != None or cY != None):
+                cv2.circle(smoothed, (cX, cY), 7, (0, 0, 255))
+                
+        if cX != None:
+            angle = 27*(cX - 320)/320    # Finds angle needed to turn
+        else:
+            angle = 'No line detected'
+        
+        #f = open('coords.txt', 'a')
+        #f.write(str(cX) + ',' + str(cY) + '\n')
+
+        #0 = x angle
+        #1 = y pos
+        if cY != None:
+            if cY > 460:
+                sys.stdout.write('stop\n')
+            else:
+                sys.stdout.write(str(angle) + '\n')
+            
+        else:
+            sys.stdout.write(str(angle) + '\n')
         sys.stdout.flush()
-        cv2.imshow('frame', thresh)
+        #print(str(angle))
+        cv2.imshow('frame', smoothed)
         cv2.imshow('img', frame)
+        cv2.imshow('thresh', thresh)
                                   
         if cv2.waitKey(1) == ord('q'):
             break
         
     cv2.destroyAllWindows()
+    #return(angle)
